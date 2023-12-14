4a68553b24dc2a32b927103f28685eac990820c1
SteveTheSloth <lucas.voigt@web.de>
2023-12-14 11:04:05 +0000

    add title to navbar, change font weight

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import styles from "@/app/page.module.css";
 import BurgerMenu from "@/components/navbar/elements/BurgerMenu";
+import Title from "./elements/Title";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <BurgerMenu />
+      <Title />
     </div>
   );
 };
---
+++ src/components/navbar/elements/Title.tsx
@@ -0,0 +1,6 @@
+import styles from "@/app/page.module.css";
+
+const Title = () => {
+  return <h1 className={styles.title}>Michas Blog um die Welt</h1>;
+};
+export default Title;
